189b56e1a0af30876013bf676577c2bd4d9e1183
Ingo Breßler <ingo.bressler@bam.de>
2019-05-07 17:02:12 +0200

    added PathSelector in *widgets* module

--- __init__.py
+++ __init__.py
@@ -5,5 +5,6 @@ from .readPDH import readPDH
 from .reBin import reBin
 from .utils import currentNBpath
 from .git import isRepo, isNBstripoutInstalled, isNBstripoutActivated, checkRepo
+from .widgets import PathSelector
 
 # vim: set ts=4 sts=4 sw=4 tw=0:
