4bbee4ef4dde0a8591215afdd527f74f9b44d303
Ingo Breßler <dev@ingobressler.net>
2021-06-23 10:34:17 +0200

    utils: moved jupyter notebook related helper to separate file/module
    - avoids unnecessary dependencies elsewhere

--- utils.py
+++ utils.py
@@ -3,10 +3,6 @@
 
 import os, sys
 import copy
-import urllib
-import json
-import ipykernel
-from notebook import notebookapp
 import locale
 import platform
 import subprocess
@@ -16,27 +12,6 @@ from pathlib import Path
 import numpy as np
 
 indent = "    "
-
-def currentNBpath():
-    """Returns the absolute path of the Notebook or None if it cannot be determined
-    NOTE: works only when the security is token-based or there is also no password
-    """
-    connection_file = os.path.basename(ipykernel.get_connection_file())
-    kernel_id = connection_file.split('-', 1)[1].split('.')[0]
-
-    for srv in notebookapp.list_running_servers():
-        try:
-            if srv['token']=='' and not srv['password']:  # No token and no password, ahem...
-                req = urllib.request.urlopen(srv['url']+'api/sessions')
-            else:
-                req = urllib.request.urlopen(srv['url']+'api/sessions?token='+srv['token'])
-            sessions = json.load(req)
-            for sess in sessions:
-                if sess['kernel']['id'] == kernel_id:
-                    return os.path.join(srv['notebook_dir'],sess['notebook']['path'])
-        except:
-            pass  # There may be stale entries in the runtime directory 
-    return None
 
 def setLocaleUTF8():
     """Fix the Jupyter locale which is not UTF-8 by default on some systems (older Windows?)."""
