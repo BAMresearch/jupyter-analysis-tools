a73f9a7c52df781bffcf388a2e5932573bbc51a7
Ingo Breßler <ingo.bressler@bam.de>
2023-02-13 17:31:42 +0100

    moved project meta data from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -19,53 +19,15 @@ def read(*names, **kwargs):
 
 
 setup(
-    name='jupyter-analysis-tools',
-    version='0.1.0',
-    license='MIT',
-    description='Common Python helpers used in data analysis notebooks (.ipynb) with GIT',
     long_description='{}\n{}'.format(
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst')),
     ),
-    author='Ingo Breßler',
-    author_email='dev@ingobressler.net',
-    url='https://github.com/BAMresearch/jupyter_analysis_tools',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Framework :: Jupyter :: JupyterLab',
-        'Topic :: Utilities',
-        'Topic :: Scientific/Engineering',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-    ],
-    project_urls={
-        'Documentation': 'https://bamresearch.github.io/jupyter_analysis_tools/',
-        'Changelog': 'https://bamresearch.github.io/jupyter_analysis_tools/changelog.html',
-        'Issue Tracker': 'https://github.com/BAMresearch/jupyter_analysis_tools/issues',
-    },
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
-    python_requires='>=3.7',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
