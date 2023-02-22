dc876ef3ad25e0bedc9ca51356040320b6aec332
ibressler <ibressler@users.noreply.github.com>
2023-02-22 20:29:12 +0000

    deploy: e871aaf76f73ef206fce7e736125a72bcd6dfaa1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev111+6474942',
+    VERSION: '0.1.1.dev112+e871aaf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
