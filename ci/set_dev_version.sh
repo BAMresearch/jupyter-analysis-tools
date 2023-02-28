84a1309702f361cc6ebae72047cd68b8d4316d69
Ingo Breßler <ingo.bressler@bam.de>
2023-02-28 18:10:27 +0100

    chore(PythonPackage): removed obsolete version number script

--- ci/set_dev_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ver="$(tbump current-version)"
-newver="${ver%.*}.$((${ver##*.}+1))"
-commits_since_release="$(git log v$ver..HEAD --oneline | wc -l)"
-commit_short="$(git rev-parse --short HEAD)"
-#devver="$(printf "%s.dev%d+%s\n" "$newver" "$commits_since_release" "$commit_short")"
-devver="$(printf "%s.dev%d\n" "$newver" "$commits_since_release")"
-tbump --only-patch --non-interactive "$devver"
