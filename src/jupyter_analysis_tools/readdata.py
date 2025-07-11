aeacfa4a949d7293f2cd3ce5e3004fddf20ecc02
Ingo Breßler <ingo.bressler@bam.de>
2025-07-11 14:41:36 +0200

    test(readdata): testdata files and testing, using pathlib instead of old os.path

--- src/jupyter_analysis_tools/readdata.py
+++ src/jupyter_analysis_tools/readdata.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 # readdata.py
 
-import os
+import json
+import warnings
+import xml.etree.ElementTree as et
+from pathlib import Path
 
 import pandas as pd
 
 
-def readdata(fn, q_range=None, read_csv_args=None, print_filename=True):
+def readdata(fpath, q_range=None, read_csv_args=None, print_filename=True):
     """Read a datafile pandas Dataframe
     extract a file_name
     select q-range: q_min <= q <= q_max
     """
+    fpath = Path(fpath)
     if print_filename:
-        print(f"Reading file '{fn}'")
+        print(f"Reading file '{str(fpath)}'")
     if read_csv_args is None:
         read_csv_args = dict()
     if "sep" not in read_csv_args:
@@ -23,10 +27,10 @@ def readdata(fn, q_range=None, read_csv_args=None, print_filename=True):
         read_csv_args.update(index_col=False)
     # print("f_read_data, read_csv_args:", read_csv_args) # for debugging
 
-    _, file_ext = os.path.splitext(fn)
+    file_ext = fpath.suffix
     if file_ext.lower() == ".pdh":  # for PDH files
         nrows = pd.read_csv(
-            fn,
+            fpath,
             skiprows=2,
             nrows=1,
             usecols=[
@@ -36,12 +40,13 @@ def readdata(fn, q_range=None, read_csv_args=None, print_filename=True):
             header=None,
         ).values[0, 0]
         read_csv_args.update(skiprows=5, nrows=nrows)
-    df_data = pd.read_csv(fn, **read_csv_args)
+    df = pd.read_csv(fpath, **read_csv_args)
 
     # select q-range
     if q_range is not None:
         q_min, q_max = q_range
-        df_data = df_data[(df_data.q > q_min) & (df_data.q < q_max)]
+        df = df[(df.q > q_min) & (df.q < q_max)]
 
-    file_name = os.path.basename(fn).split("[")[0]
-    return df_data, file_name
+    filename = fpath.stem.split("[")[0]
+    return df, filename
+
